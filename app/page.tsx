b4690aa050d4ec6b87dda5e6812988a0e6163457
RaptikFr <nightmare38@gmail.com>
2026-06-08 23:08:47 +0200

    feat(téléchargement): mise à jour de la version à v1.7.1

--- app/page.tsx
+++ app/page.tsx
@@ -122,7 +122,7 @@ export default function Home() {
               <polyline points="7 10 12 15 17 10"/>
               <line x1="12" y1="15" x2="12" y2="3"/>
             </svg>
-            Télécharger v1.7.0
+            Télécharger v1.7.1
           </a>
 
         </div>
